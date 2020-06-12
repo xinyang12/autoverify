8a4373fe316ad8dd4fbda79a9f1d2cb9ab96484e
xinyang12 <xinyang12@outlook.com>
2020-06-12 12:30:32 +0800

    update contract name

--- migrations/2_deploy_autoverify.js
+++ migrations/2_deploy_autoverify.js
@@ -1,5 +1,5 @@
-const Killable = artifacts.require("Killable");
+const Casino = artifacts.require("Casino");
 
 module.exports = function(deployer) {
-  deployer.deploy(Killable);
+  deployer.deploy(Casino);
 };
